09177739b7b9cf30d033fbfbec7cacbaa4684027
bopeara <bopeara@amazon.com>
2026-04-29 16:51:54 +1000

    Replace with kid-goals app (multi-kid support)

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ml-study-v5';
-const FILES=['./','./index.html','./study-method.html','./retrieval-practice.html','./feynman-template.html','./flashcards.html','./s3-storage-flashcards.html','./sagemaker-algorithms-flashcards.html','./hyperparameters-flashcards.html','./sagemaker-algorithm-picker.html','./3vs-big-data.html','./agent-registry-diagram.html','./manifest.json','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res}).catch(()=>caches.match('./index.html'))))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
